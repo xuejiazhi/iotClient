7d2463eb8a45613ba13278ccf1a99f364837bed7
xuejiazhi <xuejiazhi@gmail.com>
2023-12-30 20:00:26 +0800

    读取输入状态

--- modbus_test.go
+++ modbus_test.go
@@ -28,16 +28,35 @@ func Test_ReadHoldingRegisters(t *testing.T) {
 
 // 读线圈
 func Test_ReadCoils(t *testing.T) {
-	var mv m2.TcpClient
-	mv.TimeOut = 5 * time.Second
-	mv.Address = "localhost:502"
+	var tm m2.ModbusClient = &m2.TcpClient{
+		TimeOut: 5 * time.Second,
+		Address: "localhost:502",
+	}
 
-	//init
-	err := mv.InitModbus()
+	err := tm.InitModbus()
+	defer tm.Close()
 	if err != nil {
 		panic(err)
 	}
 
-	values, _ := mv.ReadCoils(99, 4)
+	//读线圈
+	values, _ := tm.ReadCoils(99, 4)
+	fmt.Println(values)
+}
+
+func Test_ReadInputStatus(t *testing.T) {
+	var tm m2.ModbusClient = &m2.TcpClient{
+		TimeOut: 5 * time.Second,
+		Address: "localhost:502",
+	}
+
+	err := tm.InitModbus()
+	defer tm.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	//读输入状态
+	values, _ := tm.ReadInputStatus(99, 4)
 	fmt.Println(values)
 }
